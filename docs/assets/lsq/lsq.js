29b802c2e7cdb718caf6c3fb3dd1f6414f1e702d
Kisung <lionks80@gmail.com>
2012-11-14 18:20:28 +0900

    documents 작성중...

--- docs/assets/lsq/lsq.js
+++ docs/assets/lsq/lsq.js
@@ -1,51 +1,36 @@
 var lsq = {};
 
-lsq.stringUtils = {};
-lsq.stringUtils.hasText = function(str) {
+lsq.string = {};
+lsq.string.hasText = function(str) {
 	return !(str == null || str.length == 0);
 };
 
-lsq.stringUtils.strFormating = function(str, arrayArguments) {
+lsq.form = {};
+lsq.form.select = {};
 
-	// 인자가 배열타입이 아닌경우 그냥 문자열 리턴
-	if (lsq.utils.isArray(arrayArguments) == false) {
+lsq.form.select.clear = function(elementId) {
+	$('#' + elementId).find('option').remove().end();
+}
 
-		if (arrayArguments != null && console) {
-			console.error('arrayArguments is not array.');
-		}
-
-		return str;
-	}
-
-	// // 인자배열이 없는 경우 그냥 문자열 리턴
-	// if (arrayArguments.length < 1) {
-	// return str;
-	// }
-
-	var tokenCount = arrayArguments.length;
-
-	for ( var token = 0; token <= tokenCount; token++) {
-		str = str.replace(new RegExp("\\{" + token + "\\}", "gi"),
-				arrayArguments[token]);
-	}
-
-	return str;
-
+lsq.form.select.addOption = function(elementId, value, text) {
+	
+	var op = document.createElement("option");
+	op.value = dataArray[i][valueProperty];
+	op.text = dataArray[i][textProperty];
+	document.getElementById(elementId).add(op);
 };
 
-lsq.formUtils = {
 
-	init_select : function(elementId, dataArray, valueProperty, textProperty) {
-		$('#' + elementId).find('option').remove().end();
 
-		for ( var i = 0; i < dataArray.length; i++) {
+lsq.form.select.bindOptions = function(elementId, dataArray, valueProperty, textProperty) {
+
+	for ( var i = 0; i < dataArray.length; i++) {
 			var op = document.createElement("option");
 			op.value = dataArray[i][valueProperty];
 			op.text = dataArray[i][textProperty];
 			document.getElementById(elementId).add(op);
 		}
 	}
-
 };
 
 lsq.utils = {};
@@ -70,16 +55,12 @@ lsq.utils.getParameterByName = function(name) {
 		return decodeURIComponent(results[1].replace(/\+/g, " "));
 };
 
-lsq.utils.getElement = function(eId) {
-	return document.getElementById(eId);
-};
-
-lsq.ajax = function ajax(method, requestUrl, params, contentType, async,
+lsq.ajax = function(method, requestUrl, data, contentType, async,
 		beforeSend, complete) {
 
 	var jsonResponse = null;
 
-	ajaxSetting = {
+	var ajaxSetting = {
 		traditional : true,
 		dataType : 'json',
 		async : false,
@@ -105,8 +86,8 @@ lsq.ajax = function ajax(method, requestUrl, params, contentType, async,
 	if (async == true) {
 		ajaxSetting.async = async;
 	}
-	if (params != null) {
-		ajaxSetting.data = params;
+	if (data != null) {
+		ajaxSetting.data = data;
 	}
 	if (contentType != null) {
 		ajaxSetting.contentType = contentType;
@@ -123,67 +104,38 @@ lsq.ajax = function ajax(method, requestUrl, params, contentType, async,
 	return jsonResponse;
 };
 
-lsq.createRestfulManager = function(baseUrl, listUrl, getUrl, createUrl,
+lsq.cm = {};
+
+lsq.cm.createRestCM = function(baseUrl, listUrl, getUrl, createUrl,
 		updateUrl, deleteUrl) {
 
-	var requestUrl_list = lsq.stringUtils.hasText(listUrl) ? listUrl : baseUrl;
-	var requestUrl_get = lsq.stringUtils.hasText(getUrl) ? getUrl : baseUrl;
-	var requestUrl_create = lsq.stringUtils.hasText(createUrl) ? createUrl
+	var requestUrl_list = lsq.string.hasText(listUrl) ? listUrl : baseUrl;
+	var requestUrl_get = lsq.string.hasText(getUrl) ? getUrl : baseUrl;
+	var requestUrl_create = lsq.string.hasText(createUrl) ? createUrl
 			: baseUrl;
-	var requestUrl_update = lsq.stringUtils.hasText(updateUrl) ? updateUrl
+	var requestUrl_update = lsq.string.hasText(updateUrl) ? updateUrl
 			: baseUrl;
-	var requestUrl_delete = lsq.stringUtils.hasText(deleteUrl) ? deleteUrl
+	var requestUrl_delete = lsq.string.hasText(deleteUrl) ? deleteUrl
 			: baseUrl;
 
-	this.list = function(params) {
-		return lsq.ajax('GET', requestUrl_list, params);
+	this.list = function(data) {
+		return lsq.ajax('GET', requestUrl_list, data);
 	};
 
-	this.get = function(key, params) {
-		return lsq.ajax('GET', requestUrl_get + "/" + key, params);
+	this.get = function(key, data) {
+		return lsq.ajax('GET', requestUrl_get + "/" + key, data);
 	};
 
 	this.create = function(data) {
-		return lsq.ajax('POST', requestUrl_create + "/" + key, params,
-				'application/json');
+		return lsq.ajax('POST', requestUrl_create, data, 'application/json');
 	};
 
 	this.update = function(key, data) {
-		return lsq.ajax('PUT', requestUrl_update + "/" + key, params,
-				'application/json');
+		return lsq.ajax('PUT', requestUrl_update + "/" + key, data, 'application/json');
 
 	};
 
-	this.remove = function(key) {
-		return lsq.ajax('DELETE', requestUrl_delete + "/" + key, params,
-				'application/json');
-	};
-
-};
-
-lsq.createSimpleRestfulManager = function(baseUrl, listUrl, getUrl,
-		saveOrUpdateUrl, deleteUrl) {
-
-	var requestUrl_list = lsq.stringUtils.hasText(listUrl) ? listUrl : baseUrl;
-	var requestUrl_get = lsq.stringUtils.hasText(getUrl) ? getUrl : baseUrl;
-	var requestUrl_saveOrUpdate = lsq.stringUtils.hasText(saveOrUpdateUrl) ? saveOrUpdateUrl
-			: baseUrl;
-	var requestUrl_delete = lsq.stringUtils.hasText(deleteUrl) ? deleteUrl
-			: baseUrl;
-
-	this.list = function(params) {
-		return lsq.ajax('GET', requestUrl_list, params);
-	};
-
-	this.get = function(key, params) {
-		return lsq.ajax('GET', requestUrl_get + "/" + key, params);
-	};
-
-	this.saveOrUpdate = function(data) {
-		return lsq.ajax('POST', requestUrl_saveOrUpdate, params);
-	};
-
-	this.remove = function(key) {
+	this.del = function(key) {
 		return lsq.ajax('DELETE', requestUrl_delete + "/" + key);
 	};
 
